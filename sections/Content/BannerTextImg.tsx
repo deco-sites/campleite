73302b3e3021f941275340789373275e53c597c4
Lucas Leite <70826073+leitecsleite@users.noreply.github.com>
2024-04-23 21:06:52 -0300

    fix-loading

--- sections/Content/BannerTextImg.tsx
+++ sections/Content/BannerTextImg.tsx
@@ -1,1 +1,10 @@
 export { default } from "deco-sites/campleite/components/ui/BannerTextImg.tsx";
+
+export function LoadingFallback() {
+    return (
+      <div style={{ height: "621px" }} class="flex justify-center items-center animate-spin">
+        <span  class="loading loading-spinner" />
+      </div>
+    );
+  }
+  
